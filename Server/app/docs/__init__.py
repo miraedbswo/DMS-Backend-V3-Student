994af18c84be2e100dfa5ce7b2fa0ad25cd2e43f
RISMME <python@istruly.sexy>
2018-10-07 14:37:05 +0900

    NEW: sample data

---
+++ Server/app/docs/__init__.py
@@ -0,0 +1,13 @@
+JWT_ACCESS_TOKEN = {
+    'name': 'Authorization',
+    'description': 'jwt access token',
+    'in': 'header',
+    'type': 'str',
+    'required': 'true'
+}
+
+SAMPLE_OBJECT_IDS = [
+    '5acddc2bc2a93f68ce96f5c4',
+    '5acddc2bc2a93f68ce96f5c9',
+    '5acddc2bc2a93f68ce96f5ce'
+]
