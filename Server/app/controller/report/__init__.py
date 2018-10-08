e5ce40cd6c9747d5528ac69368c96b6ad558e976
RISMME <python@istruly.sexy>
2018-10-08 15:06:08 +0900

    Report Baseline

---
+++ Server/app/controller/report/__init__.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from flask_restful import Api
+
+
+report_blueprint = Blueprint('report', __name__, url_prefix='/report')
+api = Api(report_blueprint)
+
+
+from .bug_report import BugReport
+api.add_resource(BugReport, '/bug/<platform>')
+
+from .facility_report import FacilityReport
+api.add_resource(FacilityReport, '/facility')
