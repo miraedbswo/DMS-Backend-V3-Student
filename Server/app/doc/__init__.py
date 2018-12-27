fbb076d7c3cc9ac9497caa4dc6faa8fb672bf6a7
Nerd-Bear <python@istruly.sexy>
2018-12-27 10:48:42 +0900

    ADD: parameter helper function and meal docs

--- Server/app/doc/__init__.py
+++ Server/app/doc/__init__.py
@@ -1,3 +1,13 @@
+def parameter(name: str, description: str, in_: str='json', type_: str='str', required: bool=True) -> dict:
+    return {
+        'name': name,
+        'description': description,
+        'in': in_,
+        'type': type_,
+        'required': required
+    }
+
+
 JWT_ACCESS_TOKEN = {
     'name': 'Authorization',
     'description': 'jwt access token',
@@ -11,3 +21,4 @@ SAMPLE_OBJECT_IDS = [
     '5acddc2bc2a93f68ce96f5c9',
     '5acddc2bc2a93f68ce96f5ce'
 ]
+
---
+++ Server/app/doc/meal/meal.py
@@ -0,0 +1,45 @@
+from app.doc import parameter
+
+MEAL_GET = {
+    'tags': ['Meal'],
+    'description': '급식 조회',
+    'parameters': [
+        parameter('date', '조회할 날짜(2019-04-20')
+    ],
+    'responses': {
+        '200': {
+            'description': '연장신청 정보 조회 성공',
+            'examples': {
+                '': {
+                    'breakfast': [
+                            '밥',
+                            '국',
+                            '김치',
+                            '반찬1',
+                            '반찬2'
+                    ],
+                    'lunch': [
+                            '밥',
+                            '국',
+                            '김치',
+                            '반찬1',
+                            '반찬2'
+                    ],
+                    'dinner': [
+                            '밥',
+                            '국',
+                            '김치',
+                            '반찬1',
+                            '반찬2'
+                    ]
+                }
+            }
+        },
+        '204': {
+            'description': '연장신청 정보 없음'
+        },
+        '403': {
+            'description': '권한 없음'
+        }
+    }
+}
