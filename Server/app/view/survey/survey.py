ad8a489bc21930577386e67494cdaa96b2663bf0
Goming <miraedbswo@gmail.com>
2018-12-27 16:07:47 +0900

    FIX survey docs

--- Server/app/view/survey/survey.py
+++ Server/app/view/survey/survey.py
@@ -1,7 +1,13 @@
-from app.doc.survey.survey import SURVEY_GET
+from app.doc.survey.survey import SURVEY_LIST_GET, SURVEY_GET
 from app.view.base_resource import SurveyResource
 
 from flasgger import swag_from
+
+
+class SurveyListView(SurveyResource):
+    @swag_from(SURVEY_LIST_GET)
+    def get(self):
+        pass
 
 
 class SurveyView(SurveyResource):
