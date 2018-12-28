21914600300cd1c12b977499715c691dffc514e5
Nerd-Bear <python@istruly.sexy>
2018-12-28 10:26:06 +0900

    UPDATE: survey api, docs

--- Server/app/view/survey/__init__.py
+++ Server/app/view/survey/__init__.py
@@ -6,8 +6,9 @@ survey_blueprint = Blueprint('survey', __name__, url_prefix='/survey')
 api = Api(survey_blueprint)
 
 
-from .survey import SurveyView
-api.add_resource(SurveyView, '/<survey_id>', defaults={'survey_id': None})
+from .survey import SurveyView, SurveyListView
+api.add_resource(SurveyListView, '/')
+api.add_resource(SurveyView, '/<survey_id>')
 
 from .question import QuestionView
 api.add_resource(QuestionView, '/question/<question_id>')
--- Server/app/view/survey/survey.py
+++ Server/app/view/survey/survey.py
@@ -1,7 +1,13 @@
-from app.doc.survey.survey import SURVEY_GET
+from app.doc.survey.survey import SURVEY_GET, SURVEY_LIST_GET
 from app.view.base_resource import SurveyResource
 
 from flasgger import swag_from
+
+
+class SurveyListView(SurveyResource):
+    @swag_from(SURVEY_LIST_GET)
+    def get(self):
+        pass
 
 
 class SurveyView(SurveyResource):
