72c0aa12fd0dc13fc4456de1cb27c312f5dd36a4
Nerd-Bear <python@istruly.sexy>
2019-01-01 23:22:52 +0900

    Notice Model

--- Server/app/model/notice.py
+++ Server/app/model/notice.py
@@ -1,1 +1,25 @@
 from app.extension import db
+
+
+class NoticeModel(db.Model):
+    __tablename__ = 'notice_model'
+    id = db.Column(db.Integer, primary_key=True)
+    post_date = db.Column(db.DateTime)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+
+
+class RuleModel(db.Model):
+    __tablename__ = 'rule_model'
+    id = db.Column(db.Integer, primary_key=True)
+    post_date = db.Column(db.DateTime)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+
+
+class QNAModel(db.Model):
+    __tablename__ = 'qna_model'
+    id = db.Column(db.Integer, primary_key=True)
+    post_date = db.Column(db.DateTime)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
