632250de22fe589d3a1123a2dcdd51b812830adb
Nerd-Bear <python@istruly.sexy>
2019-01-01 23:29:25 +0900

    Meal Model

--- Server/app/model/meal.py
+++ Server/app/model/meal.py
@@ -1,1 +1,8 @@
 from app.extension import db
+
+
+class MealModel(db.Model):
+    __tablename__ = 'meal_model'
+    date = db.Column(db.Date, primary_key=True)
+    type = db.Column(db.Enum('breakfast', 'lunch', 'dinner'))
+    meal = db.Column(db.String)     # ,로 구분하여 입력
