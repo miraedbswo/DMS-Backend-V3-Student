b4ee35b4890382251a03e3a492e3750b5c019a9d
Nerd-Bear <python@istruly.sexy>
2019-01-02 09:04:21 +0900

    Report Model

--- Server/app/model/report.py
+++ Server/app/model/report.py
@@ -1,1 +1,9 @@
 from app.extension import db
+
+
+class FacilityReport(db.Model):
+    __tablename__ = 'facility_report'
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.String, db.ForeignKey('student_model.id'))
+    room = db.Column(db.Integer)
+    content = db.Column(db.String)
