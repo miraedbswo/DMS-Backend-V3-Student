6e45674a7883f1eb129453460ee129525a056629
Nerd-Bear <python@istruly.sexy>
2019-01-03 10:09:42 +0900

    MOVE: serperate model each file

--- Server/app/model/account/student.py
+++ Server/app/model/account/student.py
@@ -1,5 +1,4 @@
 import bcrypt
-import hashlib
 import re
 from typing import Union
 
@@ -62,52 +61,6 @@ class StudentModel(db.Model):
 
     @db.validates('number')
     def validate_pw(self, key, number):
-        grade = number // 1000
-        class_ = number // 100 % 10
-        number_ = number % 100
-
-        assert 1 <= grade <= 3 and 1 <= class_ <= 4 and 1 <= number_ <= 21
-        return number
-
-    @db.validates('email')
-    def validate_email(self, key, email):
-        assert re.match(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@dsm.hs.kr$", email) is not None
-        return email
-
-
-class UnsignedStudentModel(db.Model):
-    __tablename__ = 'unsigned_student_model'
-    uuid = db.column(db.String, primary_key=True)
-    name = db.column(db.String)
-    number = db.column(db.Integer)
-    email = db.column(db.String)
-
-    def __init__(self, name, number, email):
-        self.uuid = UnsignedStudentModel.generate_uuid(email)
-        self.name = name
-        self.number = number
-        self.email = email
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    @staticmethod
-    def generate_uuid(key: str) -> str:
-        while True:
-            sha = hashlib.sha1()
-            sha.update(key.encode())
-
-            key = sha.hexdigest[:5]
-            if not UnsignedStudentModel.query.filter_by(uuid=key).first():
-                return key
-
-    @db.validates('number')
-    def validate_number(self, key, number):
         grade = number // 1000
         class_ = number // 100 % 10
         number_ = number % 100
