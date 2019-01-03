413e6f3a8c9b07b8b974795bb6b03b5baf9219f4
Goming <miraedbswo@gmail.com>
2019-01-03 16:09:38 +0900

    ADD Mocking func

--- Server/test/__init__.py
+++ Server/test/__init__.py
@@ -7,25 +7,26 @@ from app.extension import db
 
 
 class TCBase(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        self.app = create_app('testing')
-        self.client = self.app.test_client()
-        super(TCBase, self).__init__()
-
     def setUp(self):
         self.db = db
+
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
         db.create_all(app=self.app)
 
     def tearDown(self):
         db.session.remove()
         db.drop_all(app=self.app)
 
-    def _mock_response(self, content: dict, status: int=200):
+    def _mock_response(
+            self,
+            content: dict,
+            status: int = 200):
+
         mock_resp = Mock()
 
         mock_resp.status_code = status
         mock_resp.content = content
-
         return mock_resp
 
 
@@ -35,5 +36,7 @@ def check_status_code(status_code):
         def wrapper(self, *args, **kwargs):
             rv = fn(self, *args, **kwargs)
             self.assertEqual(rv.status_code, status_code)
+
         return wrapper
+
     return decorator
