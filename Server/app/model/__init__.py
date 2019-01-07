a7e388e158f96c8e3a278667c1a225eacecb4ff6
Nerd-Bear <python@istruly.sexy>
2019-01-07 11:45:47 +0900

    UPDATE: import all model in model/__init__.py

---
+++ Server/app/model/__init__.py
@@ -0,0 +1,7 @@
+from .account.student import StudentModel
+from .account.unsigned_student import UnsignedStudentModel
+
+from .apply.extension import ExtensionApplyModel
+from .apply.goingout import GoingoutApplyModel
+from .apply.music import MusicApplyModel
+from .apply.stay import StayApplyModel
