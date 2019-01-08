61bf4b4dd176c4f8e01c4d5da4a24e9b9b33df75
Goming <miraedbswo@gmail.com>
2019-01-08 10:10:00 +0900

    ADD: test_account in TCBase

--- Server/test/__init__.py
+++ Server/test/__init__.py
@@ -1,10 +1,11 @@
 from functools import wraps
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 import unittest
 from unittest.mock import Mock
 from app import create_app
 from app.extension import db
+from app.model.account.student import StudentModel
 
 
 class TCBase(unittest.TestCase):
@@ -16,18 +17,29 @@ class TCBase(unittest.TestCase):
         flask_jwt_extended.create_access_token() 함수 내부에서, 
         current_app.extensions['flask-jwt-extended'] 을 호출함.
         current_app 객체는 application context 내부에서만 사용할 수 있으므로
-        test_context를 만들어 주고 json web token을 context 내부에서 생성한다.
+        test_context 를 만들어 주고 json web token 을 app context 내부에서 생성한다.
         """
         self.test_context = self.app.app_context()
 
         with self.test_context:
-            self.jwt = create_access_token('test')
+            self.access_token = create_access_token('test')
+            self.refresh_token = create_refresh_token('test')
 
         super(TCBase, self).__init__(*args, **kwargs)
+
+    def _create_test_account(self):
+        self.test_account = StudentModel(
+            id='test',
+            pw='test',
+            name='test',
+            number=1101,
+            email='test@dsm.hs.kr'
+        ).save()
 
     def setUp(self):
         self.db = db
         db.create_all(app=self.app)
+        self._create_test_account()
 
     def tearDown(self):
         db.session.remove()
