d65fc41d7781fa02b4be05a8f7f677d182bd7901
Goming <miraedbswo@gmail.com>
2019-01-08 16:59:50 +0900

    ADD: TC get extension

---
+++ Server/test/apply/extension/test_extension_get.py
@@ -0,0 +1,33 @@
+from flask import Response
+from freezegun import freeze_time
+
+from test import TCBase, check_status_code
+from test.request import ApplyRequest
+from app.model.apply.extension import ExtensionApplyModel
+
+
+class TestGetExtension(TCBase, ApplyRequest):
+    @check_status_code(204)
+    def test_extension_default_status(self) -> Response:
+        rv: Response = self.request_extension_get(self.access_token, 11)
+
+        return rv
+
+    @check_status_code(200)
+    def test_extension_successful(self) -> Response:
+        extension_data = {
+            'classNum': 1,
+            'seatNum': 16
+        }
+
+        ExtensionApplyModel.post_extension_apply(
+            student_id='test',
+            time=11,
+            class_=extension_data['classNum'],
+            seat=extension_data['seatNum']
+        )
+
+        rv: Response = self.request_extension_get(self.access_token, 11)
+        self.assertDictEqual(extension_data, rv.json)
+
+        return rv
