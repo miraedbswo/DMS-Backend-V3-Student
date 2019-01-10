73a9fa5ff427d5f9cc583e95c7871fdbfa589bdd
Goming <miraedbswo@gmail.com>
2019-01-10 11:11:10 +0900

    ADD: Meal Test

---
+++ Server/test/meal/test_meal_get.py
@@ -0,0 +1,35 @@
+from datetime import date
+
+from flask import Response
+
+from test import TCBase, check_status_code
+from test.request import MealRequest
+from app.model.meal import MealModel
+
+
+class TestGetMeal(TCBase, MealRequest):
+    @check_status_code(200)
+    def test_get_no_content_meal(self) -> Response:
+        rv: Response = self.request_meal('2019-01-01')
+
+        for values in rv.json.values():
+            self.assertIsNone(values)
+
+        return rv
+
+    @check_status_code(200)
+    def test_get_setup_meal(self) -> Response:
+        MealModel.save_meal(date(2019, 1, 1), 1, '아침||점심||저녁')
+        rv: Response = self.request_meal('2019-01-01')
+
+        for values in rv.json.values():
+            self.assertEqual('아침' or '점심' or '저녁', values)
+
+        return rv
+
+    @check_status_code(205)
+    def test_get_meal_wrong_date(self) -> Response:
+        rv: Response = self.request_meal('2019-01-00')
+
+        return rv
+
