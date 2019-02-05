6305e65e45f199377fd3e98ba908ded0ea1f9b2f
Nerd-Bear <python@istruly.sexy>
2019-02-05 18:25:53 +0900

    fix: apply stay test

--- Server/test/__init__.py
+++ Server/test/__init__.py
@@ -24,7 +24,7 @@ class TCBase(unittest.TestCase):
         self.test_context = self.app.app_context()
 
         with self.test_context:
-            self.access_token = 'Bearer '+create_access_token('test', expires_delta=timedelta(2))
+            self.access_token = 'Bearer '+create_access_token('test', expires_delta=timedelta(100))
             self.refresh_token = create_refresh_token('test')
 
         self.test_context.push()
--- Server/test/apply/stay/test_stay_post.py
+++ Server/test/apply/stay/test_stay_post.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 from flask import Response
 from freezegun import freeze_time
 
@@ -5,9 +6,11 @@ from test import TCBase, check_status_code
 from test.request import ApplyRequest
 from app.model.apply import StayApplyModel
 
+time = str(date.today() + timedelta(6-date.today().weekday()))
+
 
 class TestPostStay(TCBase, ApplyRequest):
-    @check_status_code(200)
+    @check_status_code(201)
     def test_post_stay_successful(self):
         rv: Response = self.request_stay_post(self.access_token, 1)
 
@@ -16,8 +19,8 @@ class TestPostStay(TCBase, ApplyRequest):
 
         return rv
 
-    @freeze_time('2019-01-04 00:00:00')
-    @check_status_code(204)
+    @freeze_time(f'{time} 00:00:00')
+    @check_status_code(409)
     def test_post_stay_outside_time(self):
         rv: Response = self.request_stay_post(self.access_token, 1)
 
