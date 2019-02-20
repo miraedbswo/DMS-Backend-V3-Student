39608b65de93d70ba80455ad67be526a7a0c56c0
Nerd-Bear <python@istruly.sexy>
2019-02-20 19:24:27 +0900

    add access control expose header new access token

--- Server/app/hook/after_request.py
+++ Server/app/hook/after_request.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from flask_jwt_extended import get_jwt_identity, create_access_token
 
 from flask import Response
@@ -6,5 +8,6 @@ from flask import Response
 def new_access_token(response: Response):
     id = get_jwt_identity()
     if id is not None:
-        response.headers['New-Access-Token'] = create_access_token(identity=id)
+        response.headers['New-Access-Token'] = create_access_token(identity=id, expires_delta=timedelta(minutes=10))
+        response.headers['Access-Control-Expose-Headers'] = 'New-Access-Token'
     return response
