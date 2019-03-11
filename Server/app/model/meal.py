90a2d72536290a5062dcd3e991cec9330cd1571c
miraedbswo <miraedbswo@gmail.com>
2019-03-11 23:42:10 +0900

    delete meal useless list

--- Server/app/model/meal.py
+++ Server/app/model/meal.py
@@ -24,7 +24,7 @@ class MealModel(db.Model, BaseMixin):
 
         return {
             str(date): {
-                type_list[meal.type]: [meal.meal.split('||')]
+                type_list[meal.type]: meal.meal.split('||')
                 for meal in meal_list
             }
         }
