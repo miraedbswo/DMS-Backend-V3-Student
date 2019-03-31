5b5ac3ef22f4f9a86649259db6eb6817206bd27b
miraedbswo <miraedbswo@gmail.com>
2019-03-31 22:11:32 +0900

    [ADD] 12시 연장 신청 시간이 10시 30분까지로 증가

--- Server/app/view/apply/extension.py
+++ Server/app/view/apply/extension.py
@@ -11,7 +11,7 @@ from app.util.json_schema import json_type_validate, EXTENSION_POST_JSON
 from app.view.base_resource import ApplyResource
 
 extension_apply_start = {11: Time(17, 30), 12: Time(17, 30)}
-extension_apply_end = {11: Time(20, 30), 12: Time(22, 0)}
+extension_apply_end = {11: Time(20, 30), 12: Time(22, 30)}
 
 
 class ExtensionView(ApplyResource):
