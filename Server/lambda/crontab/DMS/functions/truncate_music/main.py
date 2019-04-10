161e1d38a5b677af58aba3cb6ac0a51296578be4
miraedbswo <miraedbswo@gmail.com>
2019-04-10 19:27:28 +0900

    [ADD] auto truncate music table cron

---
+++ Server/lambda/crontab/DMS/functions/truncate_music/main.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+
+def lambda_handler(event, context):
+    database = {
+        'host': '52.199.207.14',
+        'database': 'dms',
+        'user': 'dms',
+        'password': 'root',
+        'port': 5432
+    }
+
+    conn = psycopg2.connect(**database)
+    cursor = conn.cursor()
+
+    cursor.execute("TRUNCATE music_apply;")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
